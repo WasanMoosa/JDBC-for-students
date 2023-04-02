4a9d71f969bccfbe17b3f3258189c9080e78bba0
WasanMoosa <wasssoon@gmail.com>
2023-04-02 12:00:33 +0400

    Done

--- src/main/java/student/sql/example/Main.java
+++ src/main/java/student/sql/example/Main.java
@@ -1,9 +1,13 @@
 package student.sql.example;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+
+        // ArrayList to save students
+        ArrayList<Student> students = new ArrayList<>();
 
 
         //make connection
@@ -14,25 +18,29 @@ public class Main {
             // Read Students
             statement.executeQuery("select * from student");
             ResultSet rs = statement.getResultSet();
-            while (rs.next()) {
-                int id = rs.getInt("id");
-                String name = rs.getString("name");
-                String email = rs.getString("email");
-                System.out.println(" id: " + id + " name: " + name + " email: " + email );
 
+            while (rs.next()) {
+                Student student = new Student();
+                student.id = rs.getInt("id");
+                student.name = rs.getString("name");
+                student.email = rs.getString("email");
+                students.add(student);
+            }
+            for (Student student : students) {
+                System.out.println(" id: " + student.id + " name: " + student.name + " email: " + student.email);
             }
 
 
-            // Create Student
-            statement.execute("insert into student (name, email) values('wasan', 'wasan@gmail.com')");
-
-            // Update student
-            statement.executeUpdate("update student " +
-                                        "set email ='wasan1999@gmail.com' " +
-                                        "where id ='1'");
-
-            // delete student
-            statement.execute("delete from student where id ='1'");
+//            // Create Student
+//            statement.execute("insert into student (name, email) values('wasan', 'wasan@gmail.com')");
+//
+//            // Update student
+//            statement.executeUpdate("update student " +
+//                    "set email ='wasan1999@gmail.com' " +
+//                    "where id ='1'");
+//
+//            // delete student
+//            statement.execute("delete from student where id ='1'");
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
---
+++ src/main/java/student/sql/example/Student.java
@@ -0,0 +1,8 @@
+package student.sql.example;
+
+public class Student {
+    int id;
+    String name;
+    String email;
+
+}
